bee2011a43ba7420f98713ecccbc8aee775ff632
Zurab Kavtaradze <v-zurabk@microsoft.com>
2023-09-30 01:23:19 +0400

    services page

---
+++ Koggo/Koggo.Client/Models/ModelTemplate1.cs
@@ -0,0 +1,10 @@
+﻿namespace Koggo.Client.Models
+{
+    public class ModelTemplate1
+    {
+        public string Data { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string OwnerName { get; set; }
+    }
+}
---
+++ Koggo/Koggo.Client/Models/ModelTemplate2.cs
@@ -0,0 +1,7 @@
+﻿namespace Koggo.Client.Models
+{
+    public class ModelTemplate2
+    {
+        public string Data { get; set; }
+    }
+}
