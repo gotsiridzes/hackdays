8df0480e4b84c550ee292cff2b073684bfa570e4
Saba Gotsiridze <s.gotsiridze@altasoft.ge>
2023-09-30 19:20:28 +0400

    Add userservices filter

--- Koggo/Koggo.Domain/Models/Service.cs
+++ Koggo/Koggo.Domain/Models/Service.cs
@@ -1,37 +1,14 @@
+﻿using System.Diagnostics;
 using Koggo.Domain.Interfaces;
 
 namespace Koggo.Domain.Models;
 
 public class Service : IBaseModel
 {
-    public int Id { get; set; }
-    public string Name { get; set; } = null!;
-    public string Description { get; set; } = null!;
-    public ServiceType ServiceType { get; set; }
-    public string IconImage { get; set; } = null!;
-    public ICollection<UserService> UserServices { get; set; }
-}
-
-public enum ServiceType
-{
-    Tracktor,
-    AutoKran,
-    AmweSpiderLift,
-    AmweEuakuatori,
-    AmweKalata,
-    BetonisXelsawyo,
-    GanatebaDaVentilacia,
-    GanatebisKoshki,
-    Generatori,
-    MdzimeWonisRolikebi,
-    MdzimeWonisTeknika,
-    SasawyobeTeknika,
-    Satvirtveli,
-    SawvavisAvzi,
-    SaxliEzo,
-    TeleskopuriSatvirteli,
-    Xaracho,
-    XisDanapoteba,
-    HidravlikuriDomkrati,
-    Other
+	public int Id { get; set; }
+	public string Name { get; set; } = null!;
+	public string Description { get; set; } = null!;
+	public ServiceType ServiceType { get; set; }
+	public string IconImage { get; set; } = null!;
+	public ICollection<UserService> UserServices { get; set; }
 }
--- Koggo/Koggo.Infrastructure/Implementation/UserServiceRepository.cs
+++ Koggo/Koggo.Infrastructure/Implementation/UserServiceRepository.cs
@@ -1,4 +1,5 @@
-using System.Linq.Expressions;
+﻿using System.Linq.Expressions;
+using Koggo.Domain.Ext;
 using Koggo.Domain.Models;
 using Koggo.Infrastructure.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -14,24 +15,51 @@ public class UserServiceRepository : IUserServiceRepository
         _context = context;
     }
 
-    public async Task<List<UserService>> GetUserServicesByPageAsync(int page, int count)
-    {
-        var max = await _context.UserServices.CountAsync();
-        var maxPages = max / count;
-        var startIndex = page * count > max ? max - (max - (maxPages * count)) : page * count;
-        var takeCount = (page + 1) * count > max ? max - maxPages * count : count;
+    //public async Task<List<UserService>> GetUserServicesByPageAsync(int page, int count)
+    //{
+    //    var max = await _context.UserServices.CountAsync();
+    //    var maxPages = max / count;
+    //    var startIndex = page * count > max ? max - (max - (maxPages * count)) : page * count;
+    //    var takeCount = (page + 1) * count > max ? max - maxPages * count : count;
 
-        return await _context.UserServices
-            .AsNoTracking()
-            .Include(x => x.Service)
-            .OrderBy(x => x.Id)
-            .Skip(startIndex)
-            .Take(takeCount)
-            .ToListAsync();
+    //    return await _context.UserServices
+    //        .AsNoTracking()
+    //        .Include(x => x.Service)
+    //        .OrderBy(x => x.Id)
+    //        ;
+    //}
+
+    //TODO should implement paging
+    public async Task<List<UserService>> GetUserServicesByPageFilteredAsync(int page, int count, decimal? minPrice, decimal? maxPrice, string[]? categories)
+    {
+	    var max = await _context.UserServices.CountAsync();
+	    var maxPages = max / count;
+	    var startIndex = page * count > max ? max - (max - (maxPages * count)) : page * count;
+	    var takeCount = (page + 1) * count > max ? max - maxPages * count : count;
+		IEnumerable<UserService> userServices = _context
+		    .UserServices
+		    .AsNoTracking()
+		    .Include(x => x.Service)
+		    .OrderBy(x => x.Id);
+
+	    if (minPrice is not null)
+		    userServices = userServices.Where(x => x.Price >= minPrice);
+
+	    if (maxPrice is not null)
+		    userServices = userServices.Where(x => x.Price <= maxPrice);
+
+	    if (categories != null && categories.Length != 0)
+		    userServices = userServices.Where(x => categories.Contains(x.Service.ServiceType.ToGeo()));
+
+	    return userServices
+		    .Skip(startIndex)
+		    .Take(takeCount)
+		    .ToList();
     }
 
-    public List<UserService> GetUserServicesByPageFiltered(Expression<Func<UserService, bool>> filter)
-    {
-        throw new NotImplementedException();
-    }
+	public async Task<List<ServiceType>> ListServiceTypesAsync() =>
+	    Enum
+		    .GetValues(typeof(ServiceType))
+		    .Cast<ServiceType>()
+		    .ToList();
 }
