e49dc7ac13b9a820323559eef5dec50016551190
Shak-o <shalva.turashvili.1@btu.edu.ge>
2023-09-30 23:26:34 +0400

    we are tired

--- Koggo/Koggo.Infrastructure/Interfaces/IUserServiceRepository.cs
+++ Koggo/Koggo.Infrastructure/Interfaces/IUserServiceRepository.cs
@@ -8,4 +8,5 @@ public interface IUserServiceRepository
     //public Task<List<UserService>> GetUserServicesByPageAsync(int page, int count);
     public Task<List<UserService>> GetUserServicesByPageFilteredAsync(int page, int count, decimal? minPrice, decimal? maxPrice, string[]? categories);
     public Task<List<ServiceType>> ListServiceTypesAsync();
+    public Task<List<UserService>> GetUserServiceByIdsAndIncludes(int[] ids);
 }
