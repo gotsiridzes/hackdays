bc42ee92c21f4e721b88e4ff8a2cb4b89a9ea580
Zurab Kavtaradze <v-zurabk@microsoft.com>
2023-09-30 23:07:39 +0400

    create user service

--- Koggo/Koggo.Client/Controllers/UserServicesController.cs
+++ Koggo/Koggo.Client/Controllers/UserServicesController.cs
@@ -1,42 +1,75 @@
 using Koggo.Application.Models;
 using Koggo.Application.Services.Interface;
+using Koggo.Client.Extensions;
 using Koggo.Client.Models.Home;
+using Koggo.Domain.Models;
+using Koggo.Infrastructure;
 using Koggo.Infrastructure.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Query;
 
 namespace Koggo.Client.Controllers;
 
 public class UserServicesController : ControllerBase
 {
-	private readonly IUserServiceRepository _userServiceRepository;
-	public UserServicesController(IJwtTokenService jwtTokenService, IUserServiceRepository userServiceRepository) : base(jwtTokenService)
-	{
-		_userServiceRepository = userServiceRepository;
-	}
+    private readonly IUserServiceRepository _userServiceRepository;
+    private readonly KoggoDbContext context;
 
-	public async Task<IActionResult> Index(int page, SearchUserServices req, CancellationToken cancellationToken)
-	{
-		var isValid = ValidateToken();
-		var model = new UserServicesModel
-		{
-			TokenIsValid = isValid
-		};
+    public UserServicesController(IJwtTokenService jwtTokenService, IUserServiceRepository userServiceRepository, KoggoDbContext context) : base(jwtTokenService)
+    {
+        _userServiceRepository = userServiceRepository;
+        this.context = context;
+    }
 
-		if (!isValid)
-			return View(model);
+    public async Task<IActionResult> Index(int page, SearchUserServices req, CancellationToken cancellationToken)
+    {
+        var isValid = ValidateToken();
+        var model = new UserServicesModel
+        {
+            TokenIsValid = isValid
+        };
 
-		model.UserServices = await _userServiceRepository
-			.GetUserServicesByPageFilteredAsync(
-				page,
-				10,
-				req.MinPrice,
-				req.MaxPrice,
-				req.Categories);
+        if (!isValid)
+            return View(model);
 
-		model.ServiceTypes = await _userServiceRepository
-			.ListServiceTypesAsync();
+        model.UserServices = await _userServiceRepository
+            .GetUserServicesByPageFilteredAsync(
+                page,
+                10,
+                req.MinPrice,
+                req.MaxPrice,
+                req.Categories);
 
-		return View(model);
-	}
+        model.ServiceTypes = await _userServiceRepository
+            .ListServiceTypesAsync();
+
+        return View(model);
+    }
+
+    public async Task<IActionResult> Create()
+    {
+        var data = context.Services.ToList();
+        var model = new ServicesModel()
+        {
+            TokenIsValid = base.ValidateToken(),
+            Services = data
+        };
+        return View("CreateUserService", model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(UserServicesCreateItem userService)
+    {
+        var userInfo = HttpContext.Request.ReadJwtTokenInfo();
+        context.UserServices.Add(new Domain.Models.UserService()
+        {
+            UserId = int.Parse(userInfo.userId),
+            ServiceId = userService.ServiceId,
+            Price = userService.Price,
+            ThumbNailPhoto = userService.ThumbNailPhoto,
+            AvailableStartHour = userService.AvailableStartHour,
+            AvailableEndHour = userService.AvailableEndHour,
+        });
+        await context.SaveChangesAsync();
+        return RedirectToAction("Index", "Services");
+    }
 }
