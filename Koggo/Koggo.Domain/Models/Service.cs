ac199f7572652932bccbee5f55ff0cdb4f18642f
Zurab Kavtaradze <v-zurabk@microsoft.com>
2023-09-30 15:51:11 +0400

    seed

--- Koggo/Koggo.Domain/Models/Service.cs
+++ Koggo/Koggo.Domain/Models/Service.cs
@@ -8,12 +8,30 @@ public class Service : IBaseModel
     public string Name { get; set; } = null!;
     public string Description { get; set; } = null!;
     public ServiceType ServiceType { get; set; }
+    public string IconImage { get; set; } = null!;
     public ICollection<UserService> UserServices { get; set; }
 }
 
 public enum ServiceType
 {
     Tracktor,
-    Amwekrani,
-    ragaca
+    AutoKran,
+    AmweSpiderLift,
+    AmweEuakuatori,
+    AmweKalata,
+    BetonisXelsawyo,
+    GanatebaDaVentilacia,
+    GanatebisKoshki,
+    Generatori,
+    MdzimeWonisRolikebi,
+    MdzimeWonisTeknika,
+    SasawyobeTeknika,
+    Satvirtveli,
+    SawvavisAvzi,
+    SaxliEzo,
+    TeleskopuriSatvirteli,
+    Xaracho,
+    XisDanapoteba,
+    HidravlikuriDomkrati,
+    Other
 }
